4eb70276640bc5e03cfd56f298c1a6f19a9b82e0
Gregory V. Bilello <gregory.bilello@gmail.com>
2016-12-18 14:08:11 -0500

    Maze generates without recursive backtracking algorithm.

--- maze.js
+++ maze.js
@@ -1,11 +1,13 @@
 var rows, cols;
 var w = 40;
 var grid = [];
+var current;
 
 function setup() {
   createCanvas(400, 400);
   cols = floor(width / w);
   rows = floor(height / w);
+  frameRate(5);
 
   for (var j = 0; j < rows; j++) {
     for (var i = 0; i < cols; i++) {
@@ -13,6 +15,9 @@ function setup() {
       grid.push(cell);
     }
   }
+
+  // start at top left corner of grid
+  current = grid[0];
 }
 
 function draw() {
@@ -20,17 +25,105 @@ function draw() {
   for (var i = 0; i < grid.length; i++) {
     grid[i].show();
   }
+  current.visited = true;
+  // STEP 1
+  var next = current.checkNeighbors();
+  if (next) {
+    next.visited = true;
+    // STEP 2
+
+    // STEP 3
+    removeWalls(current, next);
+    // STEP 4
+    current = next;
+  }
+}
+
+function index(i, j) {
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1;
+  }
+  return i + j * cols;
 }
 
 function Cell(i, j) {
   this.i = i;
   this.j = j
+  // walls array order: top, right, bottom, left
+  this.walls = [true, true, true, true];
+  this.visited = false;
+
+  this.checkNeighbors = function() {
+    var neighbors = [];
+
+    var top = grid[index(i, j - 1)];
+    var right = grid[index(i + 1, j)];
+    var bottom = grid[index(i, j + 1)];
+    var left = grid[index(i - 1, j)];
+
+    if (top && !top.visited) {
+      neighbors.push(top);
+    }
+    if (right && !right.visited) {
+      neighbors.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neighbors.push(bottom);
+    }
+    if (left && !left.visited) {
+      neighbors.push(left);
+    }
+
+    if (neighbors.length > 0) {
+      var r = floor(random(0, neighbors.length));
+      return neighbors[r];
+    } else {
+      return undefined;
+    }
+  }
 
   this.show = function() {
     var x = this.i * w;
     var y = this.j * w;
     stroke(255);
-    noFill();
-    rect(x, y, w, w);
+    if (this.walls[0]) {
+      line(x, y, x + w, y); // top wall
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w); // right wall
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w); // bottom wall
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y); // left wall
+    }
+
+    if (this.visited) {
+      // color visited cell
+      noStroke();
+      fill(255, 0, 255, 100);
+      rect(x, y, w, w);
+    }
+  }
+}
+
+function removeWalls(a, b) {
+  var x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+
+  var y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
   }
 }
